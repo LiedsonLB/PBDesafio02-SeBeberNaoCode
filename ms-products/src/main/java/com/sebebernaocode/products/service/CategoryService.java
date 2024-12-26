ea78909cb995aae909dac30ce5aebea371543dc5
LiedsonLB <liedson.b9@gmail.com>
2024-12-26 12:20:17 -0300

    feat: Add Post endpoint Category resource

--- ms-products/src/main/java/com/sebebernaocode/products/service/CategoryService.java
+++ ms-products/src/main/java/com/sebebernaocode/products/service/CategoryService.java
@@ -1,7 +1,9 @@
 package com.sebebernaocode.products.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.sebebernaocode.products.entity.Category;
 import com.sebebernaocode.products.repository.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +12,9 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
 }
--- ms-products/src/main/java/com/sebebernaocode/products/web/controller/CategoryController.java
+++ ms-products/src/main/java/com/sebebernaocode/products/web/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package com.sebebernaocode.products.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sebebernaocode.products.entity.Category;
 import com.sebebernaocode.products.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RequiredArgsConstructor
@@ -15,4 +19,10 @@ import lombok.RequiredArgsConstructor;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        Category categorySaved = categoryService.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
+    }
 }
