bc4536c3bafcd2a54c268874fdb8c3cd3fd6c0a3
LiedsonLB <liedson.b9@gmail.com>
2024-12-30 16:24:26 -0300

    test: add updateCategory_WithExistingId_ReturnsCategory and deleteCategory_WithExistingId_doesNotThrowAnyException

--- ms-products/src/test/java/com/sebebernaocode/products/service/CategoryServiceTest.java
+++ ms-products/src/test/java/com/sebebernaocode/products/service/CategoryServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static com.sebebernaocode.products.common.CategoryConstants.CATEGORY;
 import static com.sebebernaocode.products.common.CategoryConstants.INVALID_CATEGORY_NAME;
 import com.sebebernaocode.products.exception.EntityNotFoundException;
+
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 import com.sebebernaocode.products.entity.Category;
 import com.sebebernaocode.products.repository.CategoryRepository;
+import com.sebebernaocode.products.web.dto.CategoryCreateDto;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,7 +42,7 @@ public class CategoryServiceTest {
     public void createCategory_WithInvalidNameNull_ThrowsException() {
         when(categoryRepository.save(INVALID_CATEGORY_NAME)).thenThrow(RuntimeException.class);
 
-        assertThatThrownBy( () -> categoryService.save(INVALID_CATEGORY_NAME) ).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> categoryService.save(INVALID_CATEGORY_NAME)).isInstanceOf(RuntimeException.class);
     }
 
     @Test
@@ -56,6 +58,35 @@ public class CategoryServiceTest {
     public void findCategoryById_WithUnexistingId_ReturnsErrorStatus404() {
         doThrow(new EntityNotFoundException("Category not found")).when(categoryRepository).findById(99L);
 
-        assertThatThrownBy( () -> categoryService.findById(99L) ).isInstanceOf(EntityNotFoundException.class);
+        assertThatThrownBy(() -> categoryService.findById(99L)).isInstanceOf(EntityNotFoundException.class);
     }
+
+    @Test
+    public void updateCategory_WithExistingId_ReturnsCategory() {
+        when(categoryRepository.save(CATEGORY)).thenReturn(CATEGORY);
+
+        Category updatedCategory = new Category(1L, "updated name");
+        when(categoryRepository.findById(anyLong())).thenReturn(Optional.of(CATEGORY));
+        when(categoryRepository.save(CATEGORY)).thenReturn(updatedCategory);
+
+        Category sut = categoryService.updateCategory(1L, new CategoryCreateDto("updated name"));
+
+        assertThat(sut).isEqualTo(CATEGORY);
+    }
+
+    @Test
+    public void updateCategory_WithUnexistingId_ReturnsErrorStatus404() {
+        doThrow(new EntityNotFoundException("Category not found")).when(categoryRepository).findById(99L);
+
+        assertThatThrownBy(() -> categoryService.updateCategory(99L, new CategoryCreateDto("updated name")))
+                .isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    public void deleteCategory_WithExistingId_doesNotThrowAnyException() {
+        when(categoryRepository.findById(1L)).thenReturn(Optional.of(CATEGORY));
+
+        assertThatCode(() -> categoryService.deleteCategory(1L)).doesNotThrowAnyException();
+    }
+
 }
